1a0e3b818b6971b517f7c5fd3e8bd5fb97841143
@francisoladipo5 <francisoladipo5@gmail.com>
2020-05-08 16:40:07 +0100

    init commit

---
+++ lib/model/movie.dart
@@ -0,0 +1,18 @@
+class Movie{
+  final int id;
+  final double popularity;
+  final String title;
+  final String backPoster;
+  final String poster;
+  final String overview;
+  final double rating;
+
+  Movie(
+      this.id,
+      this.backPoster,
+      this.overview,
+      this.popularity,
+      this.
+      )
+
+}
